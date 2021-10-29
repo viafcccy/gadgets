857548b37d50dade7e5164b22816db51d3fab16b
viafcccy <fangchenyuemail@gmail.com>
2021-10-30 00:43:09 +0800

    完成 flask 框架搭建

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # gadgets
 
 ## 部署
+
+### 环境依赖
+
+Python 3.7.0
+
+Flask 2.0.2
+
+MySQL 5.7.35
+
+Redis 3.2.1
+
---
+++ back/config/flask_config.py
@@ -0,0 +1,8 @@
+"""
+@Description :   Flask配置文件
+@Author      :   chenyu 
+@Time        :   2021/10/29 23:57:39
+"""
+
+# 是否为调试模式启动
+DEBUG = True
