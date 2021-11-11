0202c20cb4c2067e8649faadf0efe997aa9b99bf
viafcccy <fangchenyuemail@gmail.com>
2021-11-11 13:51:42 +0800

    add：引入ueditor

--- gadgets_site/app.py
+++ gadgets_site/app.py
@@ -22,6 +22,10 @@ def article():
 def login():
     return render_template('login.html')
 
+@app.route('/post')
+def post():
+    return render_template('post.html')
+
 # 启动
 if __name__ == '__main__':
     app.run()
